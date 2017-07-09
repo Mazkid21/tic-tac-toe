3f80589594b9cdb2d328af26cb558f99373d3e5f
Mazkid21 <zmaraziti@yahoo.com>
2017-07-09 13:14:07 -0600

    Click logic starting to work

--- main.js
+++ main.js
@@ -12,23 +12,42 @@
 // });
 //  });
 
-$('#box11').click(function () {
-  var classes = ['X','O','n'];
-  $('#box11').each(function(){
-    this.className = classes[($.inArray(this.className, classes)+1)%classes.length];
-  });
-});
+// $('.game').click(function () {
+//   var classes = ['X','O','n'];
+//   $('.game').each(function(){
+//     this.className = classes[($.inArray(this.className, classes)+1)%classes.length];
+//   });
+// });
 
-$('#box12').click(function () {
-  var classes = ['X','O','n'];
-  $('#box12').each(function(){
-    this.className = classes[($.inArray(this.className, classes)+1)%classes.length];
-  });
-});
+// $('.game').click(function () {
+//   var classes = ['X','O','n'];
+//   $('#box12').each(function(){
+//     this.className = classes[($.inArray(this.className, classes)+1)%classes.length];
+//   });
+// });
 
-$('#box13').click(function () {
-  var classes = ['X','O','n'];
-  $('#box13').each(function(){
-    this.className = classes[($.inArray(this.className, classes)+1)%classes.length];
-  });
+// $('.game').click(function () {
+//   var classes = ['X','O','n'];
+//   $('#box13').each(function(){
+//     this.className = classes[($.inArray(this.className, classes)+1)%classes.length];
+//   });
+// });
+
+// build three classes X O Moves, click function runs through X and O 
+
+var X = "X";
+var O = "O";
+var moves = 0; 
+
+$(".gameBoard").on("click", function () {
+	moves++;
+
+	if (moves%2 === 0) {
+		$(this).addClass(X);
+	}
+
+	else { $(this).addClass(O);
+}
+
+
 });
